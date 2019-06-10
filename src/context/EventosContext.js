95ed465d04320d541c76736e14281bacc0932a07
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-06-09 21:00:04 -0500

    done

---
+++ src/context/EventosContext.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+const EventosContext = React.createContext();
+export const EventosConsumer = EventosContext.Consumer;
+
+class EventosProvider extends Component {
+  token = 'NV433UG2CJV4UNDGVWNN';
+  ordenar = 'date'
+  state={
+      eventos:[]
+  }
+
+  obtenerEventos = async (busqueda)=>{
+    let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordenar}&token=${this.token}&locale=es_ES` 
+    
+    //Consultar eventos
+    const eventos = await axios(url)
+    this.setState({
+      eventos: eventos.data.events
+    })
+  }
+  render() {
+    return (  
+      <EventosContext.Provider 
+      value ={{
+        eventos: this.state.eventos,
+        obtenerEventos: this.obtenerEventos
+      }}>
+        {this.props.children}
+      </EventosContext.Provider>
+     );
+  }
+}
+
+export default EventosProvider;
